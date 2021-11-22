bb76f083d58b8afdec923bca373a39c915d37df3
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-22 02:59:41 +0000

    Rename boards for ease of reading. Add comments for what needs to be done and to explain what has been done.

--- run.py
+++ run.py
@@ -3,11 +3,15 @@ KEYS:
 X = Hit Battleship
 O = Miss Battleship
 ' ' = Spaces Availiable To Guess
+
+user_board = The board where the user's ships are placed
+computer_board = The board where the computer's ships are placed
+Letters_to_numbers = Allocating a number to the first 8 letter of the alphabet
 """
 # ------------------------- Game Boards
 
-guess_board = [[' '] * 9 for x in range(9)]
-hidden_board = [[' '] * 9 for x in range(9)]
+user_board = [[' '] * 9 for x in range(9)]
+computer_board = [[' '] * 9 for x in range(9)]
 
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'f': 5, 'G': 6, 'H': 7, 'I': 8}
 
@@ -25,4 +29,27 @@ def print_board(board):
         row_number += 1
 
 
-print_board(guess_board)
+"""
+def user_input():
+
+
+def create_ships():
+
+
+def place_ships():
+
+
+def check_ship_fits:
+
+
+
+def check_for_overlap():
+
+
+def hit_ship_counter():
+
+
+def turn():
+
+
+"""
