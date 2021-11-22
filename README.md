5bfcae3ad1efe1278cfd63b971951db5a64c877d
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-22 03:52:39 +0000

    Add build content images to readmefile for start of project. import random, create guess boards. Begin to write function for placing ships

--- README.md
+++ README.md
@@ -13,22 +13,29 @@ O = Miss
 
 Grid:  
   1 2 3 4 5 6 7 8 9  
-1 - - - - - - - - -
-2 - - - - - - - - -
-3 - - - - - - - - -
-4 - - - - - - - - -
-5 - - - - - - - - -
-6 - - - - - - - - -
-7 - - - - - - - - -
-8 - - - - - - - - -
+1 - - - - - - - - -  
+2 - - - - - - - - -  
+3 - - - - - - - - -  
+4 - - - - - - - - -  
+5 - - - - - - - - -  
+6 - - - - - - - - -  
+7 - - - - - - - - -  
+8 - - - - - - - - -  
 9 - - - - - - - - -  
 
 ## Build
 
-To start building this project, I felt that it was best to start by building the game baords, that way I have somrthing I can look at as I progress through the build. It also helps me with tests, as I will be able to see what is going on as I write.
+To start building this project, I felt that it was best to start by building the game baords, that way I have somrthing I can look at as I progress through the build. It also helps me with tests, as I will be able to see what is going on as I write.  
+To help me stay focused on what needs to be done I added commented out functions that will need to be created in order to get the game working properly.  
+<img src="./images/to-do.png" alt="Image of commented out function names">
 
 Link to understand string formatting
 
 https://stackoverflow.com/questions/4288973/whats-the-difference-between-s-and-d-in-python-string-formatting/56382046
 
 
+## Tests
+
+I ran various tests at regular intervals to ensure that the code I was writing was working and if it wasn't then I would always roughly know where the problem was.  
+The first test I ran was just to check that the game boards printed out ok and look good, as pictures below:  
+<img src = ./images/game-boards.png alt="Images of game boards">
--- run.py
+++ run.py
@@ -1,18 +1,26 @@
+import random
+
 """
 KEYS:
 X = Hit Battleship
 O = Miss Battleship
 ' ' = Spaces Availiable To Guess
+V = Vertical
+H = Horizontal
 
 user_board = The board where the user's ships are placed
 computer_board = The board where the computer's ships are placed
 Letters_to_numbers = Allocating a number to the first 8 letter of the alphabet
+game_ships = the sizes of the ships the player and computer will use
+
 """
 # ------------------------- Game Boards
 
 user_board = [[' '] * 9 for x in range(9)]
+user_guess_board = [[' '] * 9 for x in range(9)]
+computer_guess_board = [[' '] * 9 for x in range(9)]
 computer_board = [[' '] * 9 for x in range(9)]
-
+game_ships = [2, 3, 3, 4, 5]
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'f': 5, 'G': 6, 'H': 7, 'I': 8}
 
 
@@ -28,6 +36,39 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+def place_ships(board):
+    """
+    Function for looping through all the game_ships
+    and for checking that ships fit and don't go outside
+    the game boundaries 
+    """
+    for ship_size in game_ships:
+        while True:
+            if board == computer_board:
+                orientation, row, column = random.choice(["H", "V"]), random.randint(0, 8), random.randint(0, 8)
+                break
+
+
+
+
+def check_ship_fits(ship_size, column, row, orientation):
+    """
+    Function for checking if ship fits on board
+    """
+    if orientation == "H":
+        if column + ship_size > 9:
+            return False
+        else:
+            return True
+    else:
+        if row + ship_size > 9:
+            return False
+        else:
+            return True
+
+
+print_board(user_board)
+print_board(computer_board)
 
 """
 def user_input():
@@ -36,11 +77,10 @@ def user_input():
 def create_ships():
 
 
-def place_ships():
+
 
 
 def check_ship_fits:
-
 
 
 def check_for_overlap():
@@ -51,5 +91,7 @@ def hit_ship_counter():
 
 def turn():
 
+
+def play_game():
 
 """
